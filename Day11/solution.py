35121ae8837112ffeb5303941b048988b6fd1b5d
Rtchaik <35641847+Rtchaik@users.noreply.github.com>
2021-12-11 19:06:36 +0000

    Refactor day11

--- Day11/solution.py
+++ Day11/solution.py
@@ -27,23 +27,17 @@ def part2(data):
     return steps
 
 
-def make_step(data, flash):
+def make_step(data, flashes):
     data = data + 1
-    while len(data[data > 9]):
-        for x in range(10):
-            for y in range(10):
-                if data[y, x] > 9:
-                    data[y, x] = 0
-                    flash += 1
-                    for neighbour in neighbours((y, x)):
-                        if data[neighbour] != 0:
-                            data[neighbour] += 1
-    return data, flash
+    while len(flash:=list(zip(*np.where(data > 9)))):
+        flashes+=len(flash)
+        for item in flash:
+            data[item] = 0
+            for neighbour in neighbours(item):
+                if data[neighbour] != 0:
+                    data[neighbour] += 1
+    return data, flashes
 
 
 def neighbours(point):
-    return set.union(*[{(y, x)
-                        for x in
-                        (point[1] - 1, point[1], point[1] + 1) if 0 <= x < 10}
-                       for y in (point[0] - 1, point[0], point[0] + 1)
-                       if 0 <= y < 10]) - {point}
+    return set.union(*[{(y, x) for x in (point[1] - 1, point[1], point[1] + 1) if 0 <= x < 10} for y in (point[0] - 1, point[0], point[0] + 1) if 0 <= y < 10]) - {point}
